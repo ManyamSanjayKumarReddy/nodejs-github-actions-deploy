fc4e127de9ba599c5f9327d1b20c10883ff6b286
sanjaykumarreddymanyam <entrepreneur.sanju@gmail.com>
2026-02-19 15:33:01 +0530

    v2 added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req,res) => {
-    return res.json({msg : "\nHello from server!\n"})
+    return res.json({msg : "\nHello from server v2!\n"})
 })
 
 app.listen(PORT , () => {
